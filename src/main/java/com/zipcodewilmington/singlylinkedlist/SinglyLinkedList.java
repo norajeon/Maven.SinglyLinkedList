8eb17de6d5b3e0b7bc96bbf8b3fb3287512cd477
nora <sxj042@gmail.com>
2023-04-01 15:50:52 -0400

    add function

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,43 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Node start;
+
+    static class Node <T> {
+
+        T value;
+        Node next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public static <T> SinglyLinkedList add(SinglyLinkedList list, T value) {
+        Node node = new Node(value);
+
+        if(list.start == null) {
+            list.start = node;
+        } else {
+            Node end = list.start;
+            while (end.next != null) {
+                end = end.next;
+            }
+            end.next = node;
+//            LinkedList abc = new LinkedList();
+        }
+        return list;
+    }
+
+
+    public static <T> SinglyLinkedList remove(SinglyLinkedList list, int index) {
+
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,24 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.soap.Node;
+
+import static com.zipcodewilmington.singlylinkedlist.SinglyLinkedList.add;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testAdd() {
+        SinglyLinkedList test = new SinglyLinkedList();
+        test = add(test, 5);
+        Assert.assertEquals();
+
+
+    }
+
 }
