e3a3bd954b66d5e451c4e505e2e559cdab5bcbfe
nora <sxj042@gmail.com>
2023-04-05 17:55:44 -0400

    testing size

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -6,70 +6,133 @@ import java.util.LinkedList;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList<T> {
-    Node<T> start;
-    Node<T> last;
-
-    static class Node <T> {
-
-        T value;
-        Node<T> next;
+    Node<T> head;
 
 
 
-        public Node(T value) {
-            this.value = value;
-            this.next = null;
-        }
-    }
-
-    public static <T> SinglyLinkedList<T> add(SinglyLinkedList<T> list, T value) {
-        Node<T> node = new Node<T>(value);
-
-        if(list.start == null) {
-            list.start = node;
+    public void add(T value) {
+        if(head == null) {
+            head = new Node<T>(value);
         } else {
-            Node<T> end = list.start;
-            while (end.next != null) {
-                end = end.next;
+            Node<T> current = head;
+            while(current.getNext() != null) {
+                current = current.getNext();
             }
-            end.next = node;
-//            LinkedList abc = new LinkedList();
+            current.setNext(new Node<T>(value));
         }
-        return list;
+
     }
 
+    public Node<T> get(T value) {
+        return null;
+    }
 
-    public static <T> SinglyLinkedList<T> remove(SinglyLinkedList<T> list, int index) {
-        Node<T> currentNode = list.start;
-        Node<T> previous = null;
-
-        if (index == 0 && currentNode != null) {
-            list.start = currentNode.next;
-            return list;
+    public boolean contains(T value) {
+        Node<T> current = head;
+        while(current != null) {
+           if(current.getData().equals(value)) {
+               return true;
+           }
+           current = current.getNext();
         }
-        int count = 0;
-        while (currentNode != null) {
-            if (count == index) {
-                previous.next = currentNode.next;
-                break;
-            } else {
-                previous = currentNode;
-                currentNode = currentNode.next;
-                count++;
+        return false;
+
+    }
+
+    public void remove(String hi) {
+    }
+
+    public int find(T value) {
+        int counter = 0;
+        Node<T> current = head;
+        while(current != null) {
+            if (current.getData().equals(value)) {
+                return counter;
             }
+            current = current.getNext();
+            counter++;
         }
-
-        return list;
+        return -1;
     }
 
-    public static <T> int size(SinglyLinkedList<T> list) {
-        Node<T> currentNode = list.start;
-        int count = 0;
-        while (currentNode != null) {
-            currentNode = currentNode.next;
-            count++;
-        }
-        return count;
+    public int size() {
+        int size = 0;
+        Node<T> current = head;
+        while(current != null) {
+            current = current.getNext();
+            size++;
+            }
+        return size;
     }
+
+    public SinglyLinkedList<String> copy() {
+        return null;
+    }
+
+    public void sort() {
+    }
+
+//    static class Node <T> {
+//
+//        T value;
+//        Node<T> next;
+//
+//
+//
+//        public Node(T value) {
+//            this.value = value;
+//            this.next = null;
+//        }
+//    }
+//
+//    public static <T> SinglyLinkedList<T> add(SinglyLinkedList<T> list, T value) {
+//        Node<T> node = new Node<T>(value);
+//
+//        if(list.start == null) {
+//            list.start = node;
+//        } else {
+//            Node<T> end = list.start;
+//            while (end.next != null) {
+//                end = end.next;
+//            }
+//            end.next = node;
+////            LinkedList abc = new LinkedList();
+//        }
+//        return list;
+//    }
+//
+//
+//    public static <T> SinglyLinkedList<T> remove(SinglyLinkedList<T> list, int index) {
+//        Node<T> currentNode = list.start;
+//        Node<T> previous = null;
+//
+//        if (index == 0 && currentNode != null) {
+//            list.start = currentNode.next;
+//            return list;
+//        }
+//        int count = 0;
+//        while (currentNode != null) {
+//            if (count == index) {
+//                previous.next = currentNode.next;
+//                break;
+//            } else {
+//                previous = currentNode;
+//                currentNode = currentNode.next;
+//                count++;
+//            }
+//        }
+//
+//        return list;
+//    }
+//
+//    public static <T> int size(SinglyLinkedList<T> list) {
+//        Node<T> currentNode = list.start;
+//        int count = 0;
+//        while (currentNode != null) {
+//            currentNode = currentNode.next;
+//            count++;
+//        }
+//        return count;
+//    }
 
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,24 +1,112 @@
 package com.zipcodewilmington.singlylinkedlist;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.xml.soap.Node;
 
-import static com.zipcodewilmington.singlylinkedlist.SinglyLinkedList.add;
 
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList test;
+
+//puts whatever is in before in every single test
+    @Before
+    public void before() {
+        test = new SinglyLinkedList<String>();
+    }
 
     @Test
     public void testAdd() {
-        SinglyLinkedList test = new SinglyLinkedList();
-        test = add(test, 5);
-        Assert.assertEquals();
+        String expected = "5";
+        test.add("5");
+        Assert.assertTrue(test.contains(expected));
+    }
+    @Test
+    public void testContains() {
+        test.add("30");
+        Assert.assertTrue(test.contains("30"));
+    }
 
+    @Test
+    public void testRemove() {
+        test.add("hi");
+        test.remove("hi");
+
+        Assert.assertFalse(test.contains("hi"));
+    }
+
+    @Test
+    public void testFind1() {
+        test.add("hello");
+        test.add("whatever");
+        int expected = 1;
+        int actual = test.find("whatever");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFind2() {
+        test.add("hello");
+        test.add("");
+        int expected = -1;
+        int actual = test.find("whatever");
+
+        Assert.assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void testSize() {
+        test.add("");
+        test.add("");
+        int expected = 2;
+        int actual = test.size();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testGet() {
+        String expected = "whatever";
+        test.add("hi");
+        test.add(expected);
+        String actual = (String) test.get(1).getData();
+        Assert.assertEquals(actual, expected);
+    }
+
+//    @Test
+//    public void testCopy() {
+//        test.add("");
+//        test.add("whatever");
+//        test.add("hello");
+//
+//        SinglyLinkedList<String> list = test.copy();
+//        //deep copy so want diff addresses
+//        Assert.assertEquals(list.toString(), test.toString());
+//
+//        for(int i = 0; i < list.size(); i++) {
+//            Assert.assertEquals(list.get(i).getData(), test.get(i).getData());
+//
+//        }
+
+//    }
+
+    @Test
+    public void testSort() {
+        test.add("jaewwo");
+        test.add("hello");
+        test.add("zemmo");
+
+        test.sort();
+
+        Assert.assertEquals(test.get(0).getData(), "hello");
+
+    }
+
+
 
 }
